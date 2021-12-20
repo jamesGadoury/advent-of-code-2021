90ed3e9acd311c2913f2a3ac3efd349c97f79690
James Gadoury <gadouryjames@gmail.com>
2021-12-20 10:04:11 -0500

    Init day-17

---
+++ day-17/main.py
@@ -0,0 +1,11 @@
+# https://adventofcode.com/2021/day/17
+
+import sys
+
+def main(args):
+    if len(args) != 2:
+        print('Need to provide file input')
+        return
+
+if __name__ == '__main__':
+    main(sys.argv)
